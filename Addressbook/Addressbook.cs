a25b4c2a83634591dac4118367e90ea92b90ae2e
unknown <abhimvj575@gmail.com>
2022-12-27 09:26:25 +0530

    [Abhilash] Added: Create a Contact and Add Name ,Address,State,Zipcode

---
+++ Addressbook/Addressbook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook
+{
+    public class Addressbook
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public int ZipCode;
+        public long PhoneNumber;
+        public string Email;
+        public Addressbook(string firstname,string lastname,string address,string state,string city,int zipcode,long phonenumber,string email)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Address = address;
+            City = city;
+            State =state;
+            ZipCode = zipcode;
+            PhoneNumber = phonenumber;
+            Email = email;
+        }
+        public void displaycontact()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.WriteLine("Address: {0}", Address);
+            Console.WriteLine("city: {0}", City);
+            Console.WriteLine("state: {0}", State);
+            Console.WriteLine("zipcode: {0}",ZipCode);
+            Console.WriteLine("phone number: {0}", PhoneNumber);
+            Console.WriteLine("Email: {0}",Email);
+          
+        }
+
+
+        
+
+    }
+}
---
+++ Addressbook/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Addressbook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Addressbook program");
+
+            Addressbook ab = new Addressbook("Abhilash", "Abhi", "harohalli", "karnataka", "bangalore", 560087, 09876554321, "abhiabc75@gmail.com");
+            ab.displaycontact();
+        }
+    }
+}
